09c17f520a463fb167d2f79a6b888183920296f7
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-01-28 17:02:58 +0900

    add microcms client

---
+++ app/lib/client.server.ts
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!,
+  apiKey: process.env.MICROCMS_API_KEY!,
+});
