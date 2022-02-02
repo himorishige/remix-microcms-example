771ff32223a5f81132d47b0156480497bb9ad501
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-02 12:38:53 +0900

    refactor: env settings

--- app/libs/client.server.ts
+++ app/libs/client.server.ts
@@ -1,6 +1,14 @@
 import { createClient } from 'microcms-js-sdk';
 
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error('Missing MICROCMS_SERVICE_DOMAIN env');
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error('Missing MICROCMS_API_KEY env');
+}
+
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!,
-  apiKey: process.env.MICROCMS_API_KEY!,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
