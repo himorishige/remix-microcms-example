a0b6c708aafd0df2bc2f0a6695fb14ea74d9796d
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-05 15:45:01 +0900

    feature: og image generate function for vercel

--- app/routes/ogp/ogp[.png].ts
+++ app/routes/ogp/ogp[.png].ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import * as playwright from 'playwright-aws-lambda';
+// import playwright from 'playwright-aws-lambda';
+import chromium from 'chrome-aws-lambda';
 
 export async function loader({ request }: any): Promise<Response> {
   const headers: HeadersInit = {
@@ -13,16 +14,34 @@ export async function loader({ request }: any): Promise<Response> {
   const viewport = { width: 1200, height: 630 };
 
   // ブラウザインスタンスの生成
-  // Start the browser with the AWS Lambda wrapper (chrome-aws-lambda)
-  const browser = await playwright.launchChromium();
-  const page = await browser.newPage({ viewport });
+  let browser = null;
+  let screenshot = null;
 
-  const templateUrl = request.url.replace('ogp.png', '');
-  await page.goto(templateUrl, { waitUntil: 'domcontentloaded' });
+  try {
+    // browser = await playwright.launchChromium();
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      defaultViewport: viewport,
+      ignoreHTTPSErrors: true,
+    });
+    // const context = await browser.newContext({ viewport });
 
-  // スクリーンショットを取得する
-  const screenshot = await page.screenshot({ type: 'png' });
-  await browser.close();
+    // const page = await context.newPage();
+    const page = await browser.newPage();
+
+    const templateUrl = request.url.replace('ogp.png', '');
+    await page.goto(templateUrl || '', { waitUntil: 'domcontentloaded' });
+    // screenshot = await page.screenshot({ type: 'png' });
+    screenshot = await page.screenshot({ type: 'png' });
+  } catch (error) {
+    console.log(error);
+    throw new Error();
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
 
   return new Response(screenshot, { headers });
 }
