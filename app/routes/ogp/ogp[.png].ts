98a2b92423fb7a235091f02a279a5c745494f63e
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-05 17:20:41 +0900

    feature: og image generate function for vercel

--- app/routes/ogp/ogp[.png].ts
+++ app/routes/ogp/ogp[.png].ts
@@ -1,46 +1,42 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-// import playwright from 'playwright-aws-lambda';
 import chromium from 'chrome-aws-lambda';
+import { json } from 'remix';
 
 export async function loader({ request }: any): Promise<Response> {
   const headers: HeadersInit = {
     'Content-Type': 'image/png',
-    // can be `inline` or `attachment`
-    'Content-Disposition': `inline; filename="ogimage.png"`,
-    'x-content-type-options': 'nosniff',
+    'Content-Disposition': `inline; filename="ogp.png"`,
+    'X-Content-Type-Options': 'nosniff',
+    'Cache-Control': 's-maxage=31536000, stale-while-revalidate',
   };
 
-  // サイズの設定
   const viewport = { width: 1200, height: 630 };
 
-  // ブラウザインスタンスの生成
   let browser = null;
   let screenshot = null;
 
   try {
-    // browser = await playwright.launchChromium();
     browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
+      channel: 'chrome',
       defaultViewport: viewport,
       ignoreHTTPSErrors: true,
     });
-    // const context = await browser.newContext({ viewport });
 
-    // const page = await context.newPage();
     const page = await browser.newPage();
 
     const templateUrl = request.url.replace('ogp.png', '');
-    await page.goto(templateUrl || '', { waitUntil: 'domcontentloaded' });
-    // screenshot = await page.screenshot({ type: 'png' });
+    await page.goto(templateUrl || '', { waitUntil: 'networkidle0' });
     screenshot = await page.screenshot({ type: 'png' });
   } catch (error) {
-    console.log(error);
     throw new Error();
   } finally {
     if (browser) {
       await browser.close();
     }
+  }
+
+  if (typeof screenshot === 'undefined') {
+    throw json({ error: 'Error creating the image' }, 500);
   }
 
   return new Response(screenshot, { headers });
